3d46fe474722f3a79088cf0fe55e0e8145c008ec
pablomiklas@gmail.com <pablomiklas@gmail.com>
2019-12-29 15:30:11 +0100

    added public directory path

--- src/app.js
+++ src/app.js
@@ -1,20 +1,20 @@
+const path = require('path');
 const express = require('express');
 
 const app = express();
+// pliki z folderu public są serwwane i dostpne przez routy z ich nazwą
+// np. localhost:3000/help.html
+const publicDirectoryPath = path.join(__dirname, '../public');
 
-app.get('', (request, response) => {
-  response.send('Hello main page!');
-})
-
-app.get('/help', (request, response) => {
-  response.send('Welcome on help page');
-})
-
-app.get('/about', (request, response) => {
-  response.send('Welcome on about page');
-})
+app.use(express.static(publicDirectoryPath));
 
 app.get('/weather', (request, response) => {
+  // Mogę wysłać obiekt, tablice i zrobi się z tego json
+  // Mogę wysłac kod html
+  // response.send({
+  //   name: 'test',
+  //   age: 23,
+  // });
   response.send('Weather page');
 })
 
