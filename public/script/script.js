c98eff1dcb11fde2d786cad50fe9004cec2ee747
pablomiklas@gmail.com <pablomiklas@gmail.com>
2019-12-29 19:58:40 +0100

    added styles and script to handle data from user by form

--- public/script/script.js
+++ public/script/script.js
@@ -1,1 +1,33 @@
-console.log('hello js file script');
+const inputValue = document.querySelector('#input');
+const form = document.querySelector('#form');
+const weatherInfo = document.querySelector('#weatherInfo');
+const weatherInfo2 = document.querySelector('#weatherInfo2');
+const weatherInfo3 = document.querySelector('#weatherInfo3');
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  weatherInfo.textContent = 'loading..';
+  weatherInfo2.textContent = '';
+  weatherInfo3.textContent = '';
+
+  if (inputValue.value) {
+    fetch(`http://localhost:3000/weather?address=${inputValue.value}`)
+      .then((resp) => resp.json())
+      .then((json) => {
+        if (json.error) {
+          weatherInfo.textContent = json.error;
+          weatherInfo2.textContent = '';
+          weatherInfo3.textContent = '';
+        } else {
+          weatherInfo.textContent = json.forecast;
+          weatherInfo2.textContent = json.location;
+          weatherInfo3.textContent = json.pressure;
+        }
+      })
+      .catch((err) => {
+        weatherInfo.textContent = err;
+        weatherInfo2.textContent = '';
+        weatherInfo3.textContent = '';
+      })
+  }
+})
